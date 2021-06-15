b9f4d5c50950a5120347fab8665b19f985977576
NikyOFF <67484909+NikyOFF@users.noreply.github.com>
2021-06-15 12:19:32 +0600

    v1.0.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,42 +1,39 @@
 import { Client, VoiceState, User } from 'discord.js';
-declare namespace DiscordAdvancedEvents {
-    interface MemberSelfMuteEvenet {
-        voiceState: VoiceState;
-    }
-    interface MemberSelfDeafEvenet {
-        voiceState: VoiceState;
-    }
-    interface MemberServerMuteEvenet {
-        voiceState: VoiceState;
-        executor: User;
-        isAdmin: boolean;
-    }
-    interface MemberServerDeafEvenet {
-        voiceState: VoiceState;
-        executor: User;
-        isAdmin: boolean;
-    }
-    interface ChannelDisconnect {
-        oldVoiceState: VoiceState;
-        newVoiceState: VoiceState;
-    }
-    interface ChannelConnect {
-        oldVoiceState: VoiceState;
-        newVoiceState: VoiceState;
-    }
-    interface AdvancedEvents {
-        GUILD_MEMBER_SELF_MUTE: [MemberSelfMuteEvenet];
-        GUILD_MEMBER_SELF_DEAF: [MemberSelfDeafEvenet];
-        GUILD_MEMBER_SERVER_MUTE: [MemberServerMuteEvenet];
-        GUILD_MEMBER_SERVER_DEAF: [MemberServerDeafEvenet];
-        GUILD_CHANNEL_DISCONNECT: [ChannelDisconnect];
-        GUILD_CHANNEL_CONNECT: [ChannelConnect];
-    }
-    class AdvancedEventsClient {
-        protected client: Client;
-        constructor(client: Client);
-        on<K extends keyof AdvancedEvents>(event: K, listener: (...args: any[]) => void): AdvancedEventsClient;
-    }
+export interface MemberSelfMuteEvenet {
+    voiceState: VoiceState;
 }
-export default DiscordAdvancedEvents;
+export interface MemberSelfDeafEvenet {
+    voiceState: VoiceState;
+}
+export interface MemberServerMuteEvenet {
+    voiceState: VoiceState;
+    executor: User;
+    isAdmin: boolean;
+}
+export interface MemberServerDeafEvenet {
+    voiceState: VoiceState;
+    executor: User;
+    isAdmin: boolean;
+}
+export interface ChannelDisconnect {
+    oldVoiceState: VoiceState;
+    newVoiceState: VoiceState;
+}
+export interface ChannelConnect {
+    oldVoiceState: VoiceState;
+    newVoiceState: VoiceState;
+}
+export interface AdvancedEvents {
+    GUILD_MEMBER_SELF_MUTE: [MemberSelfMuteEvenet];
+    GUILD_MEMBER_SELF_DEAF: [MemberSelfDeafEvenet];
+    GUILD_MEMBER_SERVER_MUTE: [MemberServerMuteEvenet];
+    GUILD_MEMBER_SERVER_DEAF: [MemberServerDeafEvenet];
+    GUILD_CHANNEL_DISCONNECT: [ChannelDisconnect];
+    GUILD_CHANNEL_CONNECT: [ChannelConnect];
+}
+export declare class AdvancedEventsClient {
+    protected client: Client;
+    constructor(client: Client);
+    on<K extends keyof AdvancedEvents>(event: K, listener: (...args: any[]) => void): AdvancedEventsClient;
+}
 //# sourceMappingURL=index.d.ts.map
